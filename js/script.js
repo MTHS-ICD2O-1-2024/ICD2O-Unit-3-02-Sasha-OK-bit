98a338f30f067d203aa74023e504526b616351aa
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-03-31 17:26:31 +0000

    BAck

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * Displays an alert with the message "Hello, World!"
+ */
+function calculatehoursworkedAndhourlywage() {
+  // input
+  const TaxAmount= 3 
+  const hoursWorked = parseFloat(document.getElementById("length").value);
+  const hoursworked = parseFloat(document.getElementById("width").value)
+  const hourlyWage = parseFloat(document.getElementById("height").value);
+  let Incometax = hoursWorked * hoursworked * hourlyWage
+  let takeHomeSalary = (Incometax) / TaxAmount
+  
+  document.getElementById('answer1').innerHTML = 
+    `L * W * H: ${Incometax.toFixed(2)} <br>
+    V / 3: ${takeHomeSalary.toFixed(2)}`
+
+}
